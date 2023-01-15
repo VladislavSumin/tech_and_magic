e174c23485e4c39a8919413bb10a37fced029897
Владислав <vladislavsumin@mail.ru>
2023-01-15 21:07:16 +0300

    add bevy world inspector plugin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use bevy::app::App;
 use bevy::DefaultPlugins;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin)
         .run();
 }
