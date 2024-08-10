7bcb8dea67fa7c801fd92354a5841a849ee73e7d
Владислав <vladislavsumin@mail.ru>
2024-08-10 23:24:54 +0300

    add log crate

---
+++ core/log/src/lib.rs
@@ -0,0 +1,28 @@
+use bevy::log::{Level, LogPlugin};
+use bevy::utils::default;
+
+/// Создает [LogPlugin] с настройками по умолчанию.
+pub fn create_log_plugin() -> LogPlugin {
+    LogPlugin {
+        filter: make_log_filter(),
+        level: Level::TRACE,
+        ..default()
+    }
+}
+
+/// Возвращает строку с фильтром для логов.
+fn make_log_filter() -> String {
+    [
+        "trace",
+        "naga=info",
+        "bevy_core=info",
+        "bevy_app=info",
+        "bevy_render=info",
+        "bevy_winit=info",
+        "gilrs=info",
+        "wgpu_core=info",
+        "wgpu_hal=info",
+        "winit=info",
+    ]
+        .join(",")
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,13 @@
 mod network_plugin;
 
 use bevy::prelude::*;
+use core_log::create_log_plugin;
 use crate::network_plugin::NetworkPlugin;
 
 fn main() {
     App::new()
         .add_plugins(MinimalPlugins)
+        .add_plugins(create_log_plugin())
         .add_plugins(NetworkPlugin)
         .run();
 }
