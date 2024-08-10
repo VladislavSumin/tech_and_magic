6f5cb8dff5fda1387020d73da77e8e7c5b0a6ae3
Владислав <vladislavsumin@mail.ru>
2024-08-10 23:19:27 +0300

    client add connect

--- client/src/main_menu_plugin.rs
+++ client/src/main_menu_plugin.rs
@@ -1,5 +1,8 @@
+use std::net::UdpSocket;
+use std::time::SystemTime;
 use bevy::prelude::*;
 use bevy_egui::EguiContexts;
+use bevy_renet::renet::transport::{ClientAuthentication, NetcodeClientTransport};
 use egui::Align2;
 use crate::game_state::GameState;
 
@@ -9,12 +12,19 @@ pub struct MainMenuPlugin;
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(Update, render.run_if(in_state(GameState::MainMenu)))
+            .add_event::<ConnectEvent>()
+            .add_systems(Update, (render, connect).run_if(in_state(GameState::MainMenu)))
         ;
     }
 }
 
-fn render(mut contexts: EguiContexts) {
+#[derive(Event)]
+struct ConnectEvent;
+
+fn render(
+    mut contexts: EguiContexts,
+    mut ev_connect: EventWriter<ConnectEvent>,
+) {
     let mut host = String::new();
     host.push_str("localhost");
 
@@ -24,7 +34,26 @@ fn render(mut contexts: EguiContexts) {
             ui.label("Host");
             ui.text_edit_singleline(&mut host);
             if ui.button("Connect").clicked() {
-                //TODO
+                ev_connect.send(ConnectEvent);
             }
         });
+}
+
+fn connect(
+    mut commands: Commands,
+    mut ev_connect: EventReader<ConnectEvent>,
+) {
+    for ev in ev_connect.read() {
+        let authentication = ClientAuthentication::Unsecure {
+            server_addr: "127.0.0.1:5000".parse().unwrap(),
+            client_id: 0,
+            user_data: None,
+            protocol_id: 0,
+        };
+        let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+        let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+        let transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
+
+        commands.insert_resource(transport);
+    }
 }
