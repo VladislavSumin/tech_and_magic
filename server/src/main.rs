ee1475115834b9823605b8b78f48068694d6247c
Владислав <vladislavsumin@mail.ru>
2024-08-11 16:47:01 +0300

    add control tps

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,24 @@
+use std::time::Duration;
+use bevy::app::ScheduleRunnerPlugin;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 use core_log::create_log_plugin;
 use feature_network_server::ServerNetworkPlugin;
 
 fn main() {
+    
     App::new()
-        .add_plugins(MinimalPlugins)
+        .add_plugins(
+            MinimalPlugins
+                // Попытка сделать стабильный TPS на сервере, к сожалению это работает плохо.
+                // TODO придумать другую систему для контроля TPS.
+                .set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(1.0 / 60.0)))
+        )
         .add_plugins(create_log_plugin())
+        .add_plugins((
+            FrameTimeDiagnosticsPlugin,
+            LogDiagnosticsPlugin::default()
+        ))
         .add_plugins(ServerNetworkPlugin)
         .run();
 }
